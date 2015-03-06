28fe8392fffb5f4627f9d0461e8d994fef53a095
Kai Xuan <ngkx@KAIs-iMac.local>
2015-03-07 00:13:15 +0800

    Use correct version of php-express codes in server.js

--- server.js
+++ server.js
@@ -10,24 +10,33 @@ var intervalID;
 var hashTag = 'picturefultest';
 
 // using php-express to route php files using expressJS
-var phpExpress = require('php-express')({
+http = require('http'),
+    path = require('path'),
  
-  // assumes php is in your PATH
-  binPath: 'php'
-});
+    // require php-express and config
+    phpExpress = require('../')({
+        binPath: '/usr/bin/php' // php bin path.
+    });
+ 
+// init express
+app.set('port', process.env.PORT || 3000);
+app.use(express.bodyParser());  // body parser is required!!
+ 
  
 // set view engine to php-express
-app.set('views', './views');
+app.set('views', path.join(__dirname, 'views'));
 app.engine('php', phpExpress.engine);
 app.set('view engine', 'php');
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+ 
  
 // routing all .php file to php-express
 app.all(/.+\.php$/, phpExpress.router);
  
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('PHPExpress app listening at http://%s:%s', host, port);
+ 
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
 });
 
 /**
